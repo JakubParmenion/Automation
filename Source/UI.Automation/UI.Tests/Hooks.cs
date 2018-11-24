de0a627e93f5b27fd6c6463a76bdda447226dce6
Your Name <shaikh.farhan2009@gmail.com>
2018-11-24 18:56:54 +0000

    Farhan Shaikh

---
+++ Source/UI.Automation/UI.Tests/Hooks.cs
@@ -0,0 +1,37 @@
+﻿namespace UI.Tests
+
+{
+    using TechTalk.SpecFlow;
+    using OpenQA.Selenium.Chrome;
+    using System.Configuration;
+    using UI.Tests.Helper;
+    using UI.Tests.PO;
+
+
+    [Binding]
+    public sealed class Hooks
+    {
+        public static string baseurl = ConfigurationManager.AppSettings["BaseUrl"];
+        Calculator calculator = new Calculator();
+
+        [BeforeFeature()]
+        public static void BeforeScenario()
+        {
+            Web_Drivers.Driver = new ChromeDriver();
+            Web_Drivers.Driver.Navigate().GoToUrl(baseurl);
+
+        }
+        [BeforeScenario]
+        public void Before()
+        {
+            calculator.Maximise();
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            Web_Drivers.Driver.Quit();
+        }
+
+    }
+}
---
+++ Source/UI.Automation/UI.Tests/Steps/CalculatorSteps.cs
@@ -0,0 +1,49 @@
+﻿namespace UI.Tests.Steps
+
+{ 
+
+    using TechTalk.SpecFlow;
+    using UI.Tests.PO;
+    using FluentAssertions;
+
+
+    [Binding]
+    public class CalculatorSteps
+    {
+        Calculator calculator = new Calculator();
+
+
+        [Given(@"I am on the Parmenion Calculator page")]
+        public void GivenIAmOnTheParmenionCalculatorPage()
+        {
+            calculator.Navgiate_to_Calculator();
+        }
+        
+        [When(@"I send the ""(.*)"" Numerator ""(.*)"" Denominator")]
+        public void WhenISendTheNumeratorDenominator(string p0, string p1)
+        {
+            calculator.Click_nume_link(p0);
+            calculator.Click_deno_link(p1);
+        
+        }
+
+
+        [Then(@"I should see the NaN")]
+        public void ThenIShouldSeeThe()
+        {
+            calculator.Click_Submit_Button();
+            // the retuning text NaN should be chnaged to something more meaningful
+          
+        }
+        [Then(@"I should see the correct answer")]
+        public void ThenIShouldSeeTheCorrectAnswer()
+        {
+            calculator.Click_Submit_Button();
+            var result = calculator.GetResult();
+            result.Should().EndWithEquivalent("10");
+        }
+
+
+
+    }
+}
